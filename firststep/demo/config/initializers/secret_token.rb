ab4678e58d52a0f5d62593ac2e11fee7c423b883
Mirko Hofer <hofermir@students.zhaw.ch>
2012-02-25 15:45:25 +0100

    first ruby on rails programm

---
+++ firststep/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '9ee5ef325b1552efc049ebb48a5ea046cf7ad979ea1f106b41f46c89b97594d034e0d693dec53c0b6fdcb2234ad963954410edea65b6b3ff08683dde9fa340d5'
